73e0b7baf20f84490fbb3651b7c10f34268bc3ea
Amit Kumar Kumawat <kumawatamit587@gmail.com>
2024-04-24 08:36:36 +0530

    Book Create?Index/New

--- index.js
+++ index.js
@@ -3,22 +3,29 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const express = require("express");
-const mongoose = require("mongoose");
-const expressLayouts = require("express-ejs-layouts");
-
 const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
+const bookRouter = require("./routes/books");
+
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
-
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
-mongoose.connect(process.env.DATABASE_URL); // Fix typo here
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Mongoose"));
+
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
+
 app.listen(process.env.PORT || 3000);
