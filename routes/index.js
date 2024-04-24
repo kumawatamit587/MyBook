8e48382801e7b3472cbb040e46b1153b248f1a44
Amit Kumar Kumawat <kumawatamit587@gmail.com>
2024-04-24 14:06:10 +0530

    Final Completed

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const Book = require("../models/book");
 
 router.get("/", async (req, res) => {
+  let books;
   try {
-    books = Book.find().sort({ createAt: "desc" }).limit(10).exec();
+    books = await Book.find().sort({ createdAt: "desc" }).limit(10).exec();
   } catch {
     books = [];
   }
